79b440390d78553bb2f602a10bf0cd9caff93450
darkbuckwheat <74707285+darkbuckwheat@users.noreply.github.com>
2021-05-12 21:22:08 +0300

    первое копирование

---
+++ data/login_form.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
---
+++ forms/news.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms import BooleanField, SubmitField
+from wtforms.validators import DataRequired, URL
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    url = StringField("Ссылка на партию", validators=[URL(require_tld=True, message=None)])
+    is_private = BooleanField("Личное")
+    submit = SubmitField('Применить')
