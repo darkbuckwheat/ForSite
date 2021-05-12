983d3036db91799d1d45b69b6d249f3d81b08540
darkbuckwheat <74707285+darkbuckwheat@users.noreply.github.com>
2021-05-13 00:28:04 +0300

    Update news.py

--- forms/news.py
+++ forms/news.py
@@ -1,12 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
-from wtforms import BooleanField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms import BooleanField, SubmitField, Form
+from wtforms.validators import DataRequired, URL, InputRequired, ValidationError
+
+
+class MyValid(object):
+    def __init__(self):
+        self.messege = 'Wrong URL, try another one'
+
+    def __call__(self, form, field):
+        url = field.data
+        if url != '':
+            if url[:7] != 'https://':
+                raise ValidationError(self.messege)
 
 
 class NewsForm(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired()])
     content = TextAreaField("Содержание")
-    url = StringField("Ссылка на партию", validators=[URL(require_tld=False, message=None)])
+    url = StringField('Ссылка на партию', validators=[MyValid()])
     is_private = BooleanField("Личное")
     submit = SubmitField('Применить')
