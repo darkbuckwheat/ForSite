b69a4483ade418120377087d3626e10dc5bf2b57
darkbuckwheat <74707285+darkbuckwheat@users.noreply.github.com>
2021-05-12 23:29:03 +0300

    проверим

--- forms/news.py
+++ forms/news.py
@@ -7,6 +7,6 @@ from wtforms.validators import DataRequired, URL
 class NewsForm(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired()])
     content = TextAreaField("Содержание")
-    url = StringField("Ссылка на партию", validators=[URL(require_tld=True, message=None)])
+    url = StringField("Ссылка на партию", validators=[URL(require_tld=False, message=None)])
     is_private = BooleanField("Личное")
     submit = SubmitField('Применить')
